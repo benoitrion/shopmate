c91d66c425c3224d31692f368a37a62f416c562b
Benoit Rion <benoitrion@gmail.com>
2019-05-07 00:29:01 +0200

    Use JAWSDB_BRONZE_URL as db url

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "use_env_variable": process.env.JAWSDB_URL,
+    "use_env_variable": "JAWSDB_BRONZE_URL",
     "dialect": "mysql"
   }
 }
